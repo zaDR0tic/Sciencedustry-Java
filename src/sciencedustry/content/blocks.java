31cd60b762fc367885bc28adf2dfaa942b5691d1
Zaksen <maksimsidorkin2006@mail.ru>
2022-04-08 16:44:53 +0300

    first share for save

---
+++ src/sciencedustry/content/blocks.java
@@ -0,0 +1,11 @@
+package sciencedustry.content;
+
+import mindustry.content.Blocks;
+import mindustry.content.Items;
+import mindustry.ctype.ContentList;
+import mindustry.world.Block;
+import mindustry.world.blocks.environment.Floor;
+import mindustry.world.blocks.environment.TreeBlock;
+
+public class blocks {
+}
